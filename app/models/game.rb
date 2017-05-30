8a00e12a5209f0e8fbb100dfc9b03f7b13f1f318
solene basaia <bsolene@hotmail.fr>
2017-05-30 19:17:20 +0100

    form new game and continu list item

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,6 @@ class Game < ApplicationRecord
   belongs_to :user
   has_many :challenges
   TITLES = ['Fifa17', 'Cs', 'Madden 17', 'NBA 2K17', 'Tekken 7', 'Overwatch', 'Call of Duty: Modern Warfare' ]
-
+  validates :stake, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
 
