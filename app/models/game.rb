c830bfb07f812d1f6c793303c3740ffaf4fae72a
Jamie Walden <jamiewalden@gmail.com>
2017-05-31 10:57:44 +0100

    ready for merge

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,21 @@ class Game < ApplicationRecord
   has_many :challenges
   has_attachment :picture
 
-  TITLES = ['Fifa17', 'Cs', 'Madden 17', 'NBA 2K17', 'Tekken 7', 'Overwatch', 'Call of Duty: Modern Warfare' ]
+  GAMES = {
+    'Fifa17' => "fifa-banner-2.jpg",
+    'Madden 17' => "madden-17.jpg",
+    'NBA 2K17' => "nba-card.jpg",
+    'Tekken 7' => "tekken-card.jpg",
+    'Overwatch' => "overwatch-card.jpg",
+    'Call of Duty: Modern Warfare' => "cod-card.jpg",
+    'Forza Horizon 3' => "forza-card.jpg"
+  }
+
   validates :stake, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  def picture_file_name
+    return GAMES[name]
+  end
+
 end
 
